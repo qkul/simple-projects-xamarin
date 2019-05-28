fe0ebc6e8def1352401c4618c9c8941467f732d7
Andrei Kulbeda <Andrei_Kulbeda@epam.com>
2019-05-28 15:53:01 +0300

    menu in android

---
+++ HamburgerMenu/HamburgerMenu/MainActivity.cs
@@ -0,0 +1,51 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Support.V4.Widget;
+using V7Toolbar = Android.Support.V7.Widget.Toolbar;
+using Android.Support.Design.Widget;
+using Android.Views;
+
+namespace HamburgerMenu
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        DrawerLayout drawerLayout;
+        NavigationView navigationView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            // Set our view from the "main" layout resource  
+            SetContentView(Resource.Layout.activity_main);
+            drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            // Create ActionBarDrawerToggle button and add it to the toolbar  
+            //var toolbar = FindViewById<V7Toolbar>(Resource.Id.toolbar);
+            //SetSupportActionBar(toolbar);
+            var drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, Resource.String.drawer_open,
+                Resource.String.drawer_close);
+            drawerLayout.SetDrawerListener(drawerToggle);
+            drawerToggle.SyncState();
+            navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            setupDrawerContent(navigationView); //Calling Function  
+        }
+
+        void setupDrawerContent(NavigationView navigationView)
+        {
+            navigationView.NavigationItemSelected += (sender, e) =>
+            {
+                e.MenuItem.SetChecked(true);
+                drawerLayout.CloseDrawers();
+            };
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            navigationView.InflateMenu(Resource.Menu.item_menu); 
+            return true;
+        }
+    }
+}
